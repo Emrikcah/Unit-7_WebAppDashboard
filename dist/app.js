a521fb12b0f4d996515f0d22d67d34d3cc2ccd0e
Emrikcah <curryandr3@gmail.com>
2020-04-08 11:51:24 -0400

    added layout and js files

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+// Mobile Menu
+const btn = document.querySelector('.mobile-menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+
+//Click the mobile menu btn and toggle mobile-nav
+btn.addEventListener('click',() =>{
+    mobileNav.classList.toggle('show');
+});
