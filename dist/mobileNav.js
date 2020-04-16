6ba0f65d87116256f00e8db7e90a24b794390e0d
Emrikcah <curryandr3@gmail.com>
2020-04-16 15:35:44 -0400

    working with fixed header and alert positioning

--- dist/mobileNav.js
+++ dist/mobileNav.js
@@ -3,9 +3,28 @@ const btn = document.querySelector('.mobile-menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
 
 //Click the mobile menu btn and toggle mobile-nav
-btn.addEventListener('click',() =>{
+btn.addEventListener('click', () => {
     mobileNav.classList.toggle('show');
 });
 // Mobile menu end
 
+//Alert
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML =
+    `
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
 
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    console.log(element);
+    
+    if (element.classList.contains("alert-banner-close")) {
+        alert.style.display = "none"
+    }
+});
