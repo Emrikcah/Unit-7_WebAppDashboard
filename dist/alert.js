780f10a595dad346c1332ed68da12da585004aed
Emrikcah <curryandr3@gmail.com>
2020-04-17 08:13:35 -0400

    added new js files to separate code

---
+++ dist/alert.js
@@ -0,0 +1,21 @@
+//Alert
+const alertBanner = document.getElementById("alert");
+// create the html for the banner
+alertBanner.innerHTML =
+    `
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    console.log(element);
+    
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+    
+});
