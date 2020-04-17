0900725f515be09bdce57354d354e1ce8845ea2f
Emrikcah <curryandr3@gmail.com>
2020-04-17 08:14:23 -0400

    removed code for alert and placed in own file

--- dist/mobileNav.js
+++ dist/mobileNav.js
@@ -8,23 +8,3 @@ btn.addEventListener('click', () => {
 });
 // Mobile menu end
 
-//Alert
-const alertBanner = document.getElementById("alert");
-// create the html for the banner
-alertBanner.innerHTML =
-    `
-<div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
-<p class="alert-banner-close">x</p>
-</div>
-`
-
-alertBanner.addEventListener('click', e => {
-    const element = e.target;
-    console.log(element);
-    
-    if (element.classList.contains("alert-banner-close")) {
-        alert.style.display = "none"
-    }
-});
