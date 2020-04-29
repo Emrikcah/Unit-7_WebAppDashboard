e10bc8b315f4a2195322d8067138af5f11a86331
Emrikcah <curryandr3@gmail.com>
2020-04-29 13:24:59 -0400

    removed console.logs

--- dist/mobileNav.js
+++ dist/mobileNav.js
@@ -2,6 +2,7 @@
 const btn = document.querySelector('.mobile-menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
 
+
 //Click the mobile menu btn and toggle mobile-nav
 btn.addEventListener('click', () => {
     mobileNav.classList.toggle('show');
--- dist/msg.js
+++ dist/msg.js
@@ -1,20 +1,18 @@
-const user = document .getElementById( "userField" );
-console.log(user);
+const user = document.getElementById("userField");
 
-const message = document .getElementById( "messageField" );
-console.log(message);
+const message = document.getElementById("messageField");
 
-const send = document .getElementById( "send" );
+const send = document.getElementById("send");
 
-send.addEventListener( 'click' , () => {
-    // ensure user and message fields are filled out
-    if (user.value === "" && message.value === "" ) {
-    alert( "Please fill out user and message fields before sending" );
-    } else if (user.value === "" ) {
-    alert( "Please fill out user field before sending" );
-    } else if (message.value === "" ) {
-    alert( "Please fill out message field before sending" );
-    } else {
-    alert( `Message successfully sent to: ${user.value}` );
-    }
-    });
+send.addEventListener("click", () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
